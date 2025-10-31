1e4fd1f8bf8aeb54c731d380704355d226e6f62f
LONECODER1 <adityagupta1112004vns@gmail.com>
2025-10-31 13:07:59 +0530

    added reset logic and UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,32 +14,36 @@ import Home from "./pages/Home.jsx";
 import Cart from "./pages/Cart.jsx";
 import Payment from "./pages/Payment";
 import Dashboard from "./pages/Dashboard";
+import RequestReset from "./pages/RequestReset";
+import ResetPassword from "./pages/ResetPassword";
+
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      
       <BrowserRouter>
-            <Navbar />
-      <div className="pt-[50px]">
-        <CartProvider>
-        
-        <Routes>
-          <Route path="/" element={<Index />} />
+        <Navbar />
+        <div className="pt-[50px]">
+          <CartProvider>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/browse" element={<Browse />} />
+              <Route path="/product/:id" element={<ProductDetailsPage />} />
+              <Route path="/CartHome" element={<Home />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/payment" element={<Payment />} />
+              <Route path="/sell" element={<Sell />} />
+              <Route path="/dashboard" element={<Dashboard />} />
 
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/browse" element={<Browse />} />
-          <Route path="/product/:id" element={<ProductDetailsPage />} />
-          <Route path="/CartHome" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/payment" element={<Payment />} />
-          <Route path="/sell" element={<Sell />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-        </CartProvider>
+              {/* Added password reset routes */}
+              <Route path="/request-reset" element={<RequestReset />} />
+              <Route path="/reset-password/:token" element={<ResetPassword />} />
+            </Routes>
+          </CartProvider>
         </div>
       </BrowserRouter>
     </TooltipProvider>
